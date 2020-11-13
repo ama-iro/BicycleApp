2ed9571983a055b675bc77792bb6d4a12ee283e9
Masato <okb.theday.mst@gmail.com>
2020-11-13 17:34:44 +0900

    home about termsの表示テストまで

--- BicycleApp/spec/system/static_pages_spec.rb
+++ BicycleApp/spec/system/static_pages_spec.rb
@@ -8,37 +8,31 @@ RSpec.describe "StaticPages", type: :system do
       end
 
       it "アプリ名の文字列が存在することを確認" do
-        expect(page).to have_content 'タイトル名'
+        expect(page).to have_content 'explain'
       end
 
     end
   end
 
-  # describe "ヘルプページ" do
-  #   before do
-  #     visit about_path
-  #   end
-  #
-  #   it "クックログとは？の文字列が存在することを確認" do
-  #     expect(page).to have_content 'クックログとは？'
-  #   end
-  #
-  #   it "正しいタイトルが表示されることを確認" do
-  #     expect(page).to have_title full_title('クックログとは？')
-  #   end
-  # end
-  #
-  # describe "利用規約ページ" do
-  #   before do
-  #     visit use_of_terms_path
-  #   end
-  #
-  #   it "利用規約の文字列が存在することを確認" do
-  #     expect(page).to have_content '利用規約'
-  #   end
-  #
-  #   it "正しいタイトルが表示されることを確認" do
-  #     expect(page).to have_title full_title('利用規約')
-  #   end
-  # end
+  describe "ヘルプページ" do
+    before do
+      visit about_path
+    end
+
+    it "MTTBとは？の文字列が存在することを確認" do
+      expect(page).to have_content 'MTTBとは？'
+    end
+
+  end
+
+  describe "利用規約ページ" do
+    before do
+      visit use_of_terms_path
+    end
+
+    it "利用規約の文字列が存在することを確認" do
+      expect(page).to have_content '利用規約'
+    end
+
+  end
 end
