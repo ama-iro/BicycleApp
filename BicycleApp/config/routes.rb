60182031b7096a229ef0b10085d8bd0e82c3e051
Masato <okb.theday.mst@gmail.com>
2020-11-16 14:36:11 +0900

    twitter認証用のカラム削除

--- BicycleApp/config/routes.rb
+++ BicycleApp/config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'users/new'
+  get 'users/show'
+  get 'users/index'
   devise_for :users
   root 'static_pages#home'
   get :about,        to: 'static_pages#about'
   get :use_of_terms, to: 'static_pages#terms'
+  get :signup,       to: 'users#new'
+  resources :users
  end
