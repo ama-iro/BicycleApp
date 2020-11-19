15209c4c7d3e67b2ae4f5642340e491ce90dfeee
Masato <okb.theday.mst@gmail.com>
2020-11-19 23:30:56 +0900

    各カラムpresence:true追加

--- BicycleApp/app/models/user.rb
+++ BicycleApp/app/models/user.rb
@@ -1,17 +1,19 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
 
   validates :name, presence: true
+  validates :gender, presence: true
+  validates :birthday, presence: true
   validates :introduction, length: { maximum: 200 }
 
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
          # :omniauthable, omniauth_providers:[:twitter]
 
 
