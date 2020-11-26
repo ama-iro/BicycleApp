cc25e6ff2adc198bb44b26a60839d67416320529
Masato <okb.theday.mst@gmail.com>
2020-11-26 17:19:55 +0900

    Postにバリデーション追加

--- BicycleApp/app/models/post.rb
+++ BicycleApp/app/models/post.rb
@@ -1,3 +1,9 @@
 class Post < ApplicationRecord
+  validates :
+  validates :area, presence: true
+  validates :place, presence: true
+  validates :required_time, presence: true
+  validates :description, length: { maximum: 400 }
+
   belongs_to :user
 end
