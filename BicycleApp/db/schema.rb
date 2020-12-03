1956716b32a390bb9c011dd7fd2f6e862df2f84f
Masato <okb.theday.mst@gmail.com>
2020-12-03 10:29:29 +0900

    adminカラム追加、ヘッダーにuser一覧追加、seedにadminのuserwo

--- BicycleApp/db/schema.rb
+++ BicycleApp/db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_19_081149) do
+ActiveRecord::Schema.define(version: 2020_11_30_144209) do
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "area"
+    t.string "place"
+    t.integer "required_time"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id", "created_at"], name: "index_posts_on_user_id_and_created_at"
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,8 +36,10 @@ ActiveRecord::Schema.define(version: 2020_11_19_081149) do
     t.string "name", default: "", null: false
     t.integer "gender", null: false
     t.date "birthday"
+    t.boolean "admin", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "posts", "users"
 end
