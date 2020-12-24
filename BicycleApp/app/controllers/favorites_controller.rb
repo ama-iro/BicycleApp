4a8e99efa0f4e227004bf02427f5ff9da2f0d519
Masato <okb.theday.mst@gmail.com>
2020-12-24 23:04:14 +0900

    comments,favoriteコントローラーに通知を生成する処理作成

--- BicycleApp/app/controllers/favorites_controller.rb
+++ BicycleApp/app/controllers/favorites_controller.rb
@@ -1,12 +1,19 @@
 class FavoritesController < ApplicationController
 
   def create
+    @user = Post.find(params[:post_id])
     @favorite = Favorite.new(
       user_id: current_user.id,
       post_id: params[:post_id]
     )
     @favorite.save
     redirect_to("/posts/#{params[:post_id]}")
+
+    # 自分以外のユーザーからお気に入り登録があったときのみ通知を作成
+    if @user != current_user
+      @user.notifications.create(post_id: @post.id, variety: 1,
+                                 from_user_id: current_user.id)
+      @user.update_attribute(:notification, true)
   end
 
   def destroy
--- BicycleApp/config/routes.rb
+++ BicycleApp/config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
 
   resources :posts
   resources :comments, only: [:create, :destroy]
+  resources :notifications, only: :index 
 
 
   root 'static_pages#home'
