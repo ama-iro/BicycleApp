c918772ab15ec63f64cf3bbdad4aecc798d8c61e
Masato <okb.theday.mst@gmail.com>
2021-01-16 00:13:01 +0900

    プロフィール画面上部作成

--- BicycleApp/app/helpers/users_helper.rb
+++ BicycleApp/app/helpers/users_helper.rb
@@ -7,4 +7,11 @@ module UsersHelper
     end
     return (Date.today.strftime("%Y%m%d").to_i - birthdayStr.to_i) / 10000
   end
+
+
+  def gravatar_for(user, size: 80, cl: "gravatar")
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?d=mp&?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: cl)
+  end
 end
